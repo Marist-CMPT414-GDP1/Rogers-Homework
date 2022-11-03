5920f77cd683e87bc2e9534592567fbc26853ad3
Christopher Rogers <Christopher.Rogers1@marist.edu>
2022-11-03 14:46:56 -0400

    Lab 1

--- PongLabs/Bat.h
+++ PongLabs/Bat.h
@@ -1,22 +1,47 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+
 using namespace sf;
+
 class Bat
 {
 private:
 	Vector2f m_Position;
+
 	// A RectangleShape object
 	RectangleShape m_Shape;
+
 	float m_Speed = 1000.0f;
-	bool m_MovingRight = false;
+
+	bool m_MovingUp = false;
+	bool m_MovingDown = false;
 	bool m_MovingLeft = false;
+	bool m_MovingRight = false;
+
+
 public:
 	Bat(float startX, float startY);
+
 	FloatRect getPosition();
+
 	RectangleShape getShape();
+
+	void moveUp();
+
+	void moveDown();
+
 	void moveLeft();
+
 	void moveRight();
+
 	void stopLeft();
+
 	void stopRight();
+
+	void stopUp();
+
+	void stopDown();
+
 	void update(Time dt);
+
 };
